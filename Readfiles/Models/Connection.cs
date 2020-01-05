60f4c2425ebf79fc999415339f5da138a27f7877
Francisco Martillo <franciscom119@gmail.com>
2020-01-05 11:51:24 -0400

    Generar conexión a SQL Server

---
+++ Readfiles/Models/Connection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Readfiles.Models
+{
+    class Connection
+    {
+        /// <summary>
+        /// Conexión con base de datos SQL Server. 
+        ///<return>
+        /// Devuelve conexión.
+        ///</return>
+        /// </summary>
+        public string Connect()
+        {
+            SqlConnectionStringBuilder con = new SqlConnectionStringBuilder
+            {
+                ["Data Source"] = "FRANCISCO-PC",
+                ["Initial Catalog"] = "Interfaz_AX_DOM",
+                ["User ID"] = "laboratorio",
+                ["Password"] = "ccs_joincic60"
+            };
+            return con.ConnectionString;
+        }
+    }
+}
